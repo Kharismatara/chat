a24cecc170f416425abc7d2d8c2ef9c727f25043
Ilham Prasetya <ilham.prasetya@nawadata.id>
2023-06-06 19:09:26 +0700

    delete

--- server/index.js
+++ server/index.js
@@ -1,13 +1,29 @@
-const app = require("express")();
-const http = require("http").createServer(app);
+const express = require("express");
+const app = express();
+const http = require("http").Server(app);
 const io = require("socket.io")(http);
 
+// Menyajikan halaman statis index.html
+app.use(express.static(__dirname + "/public"));
+
+// Event saat klien terhubung
 io.on("connection", (socket) => {
-  socket.on("message", ({ name, message }) => {
-    io.emit("message", { name, message });
+  console.log("Klien terhubung");
+
+  // Event saat menerima pesan baru
+  socket.on("message", (message) => {
+    console.log("Pesan diterima: " + message);
+    io.emit("message", message); // Mengirim pesan ke semua klien yang terhubung
+  });
+
+  // Event saat klien terputus
+  socket.on("disconnect", () => {
+    console.log("Klien terputus");
   });
 });
 
-http.listen(5173, function () {
-  console.log("listening on port 5175");
+// Menjalankan server
+const port = 3000;
+http.listen(port, () => {
+  console.log(`Server berjalan di http://localhost:${port}`);
 });
