a568b05813f601d671db5042419c60043a3c6f9b
ilse31 <ilhamprazsetyo33@gmail.com>
2023-06-06 19:59:34 +0700

    fiks chat

--- server/index.js
+++ server/index.js
@@ -3,27 +3,19 @@ const app = express();
 const http = require("http").Server(app);
 const io = require("socket.io")(http);
 
-// Menyajikan halaman statis index.html
-app.use(express.static(__dirname + "/public"));
-
-// Event saat klien terhubung
 io.on("connection", (socket) => {
-  console.log("Klien terhubung");
+  console.log("User connected");
 
-  // Event saat menerima pesan baru
-  socket.on("message", (message) => {
-    console.log("Pesan diterima: " + message);
-    io.emit("message", message); // Mengirim pesan ke semua klien yang terhubung
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
   });
 
-  // Event saat klien terputus
-  socket.on("disconnect", () => {
-    console.log("Klien terputus");
+  socket.on("chat message", (msg) => {
+    console.log("Received message:", msg);
+    socket.broadcast.emit("chat message", msg); // Mengirim pesan ke semua klien terhubung kecuali pengirim
   });
 });
 
-// Menjalankan server
-const port = 3000;
-http.listen(port, () => {
-  console.log(`Server berjalan di http://localhost:${port}`);
+http.listen(3001, () => {
+  console.log("Server started on port 3001");
 });
